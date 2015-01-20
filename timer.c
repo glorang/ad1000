52171beeb9f20a236ff2f7788f89ce8b5221e372
Geert Lorang <geert@lorang.be>
2015-01-20 22:38:54 +0100

    Re-implemented playlist's current / total tracks

--- timer.c
+++ timer.c
@@ -26,21 +26,23 @@ volatile sig_atomic_t paused;
 
 /* toggle pause */
 void toggle_pause();
+int writeReadSocket(int sock, char *message, char *server_reply);
 
 int main(int argc, char *argv[]) {
 
         int method = 0;
         
-        /* only show title once */
-        int title_shown = 0;
+        /* only show startup info once */
+        int info_shown = 0;
 
         /* socket vars */
         int sock, i;
         struct sockaddr_in server;
-        char server_reply[2000] = { 0x00 };
+        char *server_reply;
+        server_reply = calloc(1, 2000); 
 
         /* cJSON vars to parse JSON */
-        cJSON *c_root, *c_id, *c_result, *c_time, *c_milli, *c_minutes, *c_seconds, *c_type, *c_label, *c_item, *c_arraysub, *c_artist, *c_artistarray, *c_title;
+        cJSON *c_root, *c_id, *c_result, *c_time, *c_milli, *c_minutes, *c_seconds, *c_type, *c_label, *c_item, *c_arraysub, *c_artist, *c_artistarray, *c_title, *c_items;
 
         /* Open syslog */
         openlog("timer", LOG_PID|LOG_CONS, LOG_USER);
@@ -64,50 +66,104 @@ int main(int argc, char *argv[]) {
         connect(sock , (struct sockaddr *)&server , sizeof(server));
 
          
-        /* Main loop - increase timing counter */
+        /* Main loop */
         while(stop == 0) { 
 
-                /* Marquee title */
-                if(title_shown == 0) { 
-                        title_shown = 1; 
+                /* At startup we show some info about the media that is starting */
+                /* For Audio : current_track / total_tracks ; artist - title */
+                /* For Video : filename */
+
+                if(info_shown == 0) { 
+                        info_shown = 1; 
                         char message[] = "{\"jsonrpc\": \"2.0\", \"method\": \"Player.GetActivePlayers\", \"id\": 1}";
-                        if(send(sock, message, strlen(message), 0) < 0) {
-                                syslog(LOG_ERR, "Could not write to socket");
-                        }
+                        writeReadSocket(sock, message, server_reply); 
 
-                        if(recv(sock , server_reply , 2000 , 0) < 0) {
-                                syslog(LOG_ERR, "could not retrieve data from socket");
-                                break;
-                        }
-
-                        /* Parse JSON data */
+                        /* Parse JSON data to determine if we're playing audio or video */
                         c_root = cJSON_Parse(server_reply);
+
                         /* Get player type */
                         if(c_root != NULL) c_result = cJSON_GetObjectItem(c_root,"result");
                         if(c_result != NULL) c_arraysub = cJSON_GetArrayItem(c_result, 0);
                         if(c_arraysub != NULL) c_type = cJSON_GetObjectItem(c_arraysub, "type");
 
                         if(c_type != NULL) {
+                                if(strcmp(c_type->valuestring, "audio") == 0) { method = METHOD_AUDIO; } 
+                                if(strcmp(c_type->valuestring, "video") == 0) { method = METHOD_VIDEO; } 
+                        }
 
-                                /* if type is audio, ask for title and artist */
-                                if(strcmp(c_type->valuestring, "audio") == 0) {
-                                        method = METHOD_AUDIO;
-                                        char message[] = "{\"jsonrpc\": \"2.0\", \"method\": \"Player.GetItem\", \"params\": { \"properties\": [\"title\", \"artist\"], \"playerid\": 0 }, \"id\": \"AudioGetItem\"}";
-                                        send(sock, message, strlen(message), 0);
+                        cJSON_Delete(c_root);
+
+                        /* if type is audio, show current_track / total_tracks and after that marquee artist - title */
+                        if(method == METHOD_AUDIO) {
+                                char playlist_query[] = "{\"jsonrpc\": \"2.0\", \"method\": \"Playlist.GetItems\", \"params\": { \"properties\": [],  \"playlistid\": 0 }, \"id\": 1}";
+                                writeReadSocket(sock, playlist_query, server_reply);
+                        
+                                /* Parse JSON data */
+                                c_root = cJSON_Parse(server_reply);
+                                if(c_root != NULL) c_result = cJSON_GetObjectItem(c_root,"result");
+                                if(c_result != NULL) c_items = cJSON_GetObjectItem(c_result,"items");
+
+                                /* Store ids returned by API in track_ids[] */
+                                int track_count, i;
+                                track_count = cJSON_GetArraySize(c_items);
+                                int track_ids[track_count]; 
+
+                                for (i=0;i<track_count;i++) {
+                                                cJSON *subitem=cJSON_GetArrayItem(c_items,i);
+                                                cJSON *item = cJSON_GetObjectItem(subitem, "id");
+                                                track_ids[i] = (item == NULL) ? 1 : item->valueint;
                                 }
 
-                                /* if type is video, ask for title */
-                                if(strcmp(c_type->valuestring, "video") == 0) {
-                                        method = METHOD_VIDEO;
-                                        char message[] = "{\"jsonrpc\": \"2.0\", \"method\": \"Player.GetItem\", \"params\": { \"properties\": [\"title\"], \"playerid\": 1 }, \"id\": \"VideoGetItem\"}";
-                                        send(sock, message, strlen(message), 0);
+                                cJSON_Delete(c_root);
+
+                                char current_item_query[] = "{\"jsonrpc\": \"2.0\", \"method\": \"Player.GetItem\", \"params\": { \"properties\": [\"title\", \"artist\"], \"playerid\": 0 }, \"id\": \"AudioGetItem\"}";
+                                writeReadSocket(sock, current_item_query, server_reply);
+
+                                /* Parse JSON data */
+                                c_root = cJSON_Parse(server_reply);
+                                if(c_root != NULL) c_result = cJSON_GetObjectItem(c_root,"result");
+                                if(c_result != NULL) c_item = cJSON_GetObjectItem(c_result,"item");
+                                if(c_item != NULL) c_id = cJSON_GetObjectItem(c_item,"id");
+                                if(c_item != NULL) c_artist = cJSON_GetObjectItem(c_item,"artist");
+                                if(c_item != NULL) c_title = cJSON_GetObjectItem(c_item,"title");
+                                if(c_artist != NULL) c_artistarray = cJSON_GetArrayItem(c_artist, 0);
+
+                                /* Display playlist position */
+                                if(c_id != NULL) {
+                                        int current_track = 1;
+                                        for(i=0;i<track_count;i++) {
+                                                if(c_id->valueint == track_ids[i]) {
+                                                        current_track = i+1;
+                                                        break;
+                                                }
+                                        }
+
+                                        char disp[4] = { 0x00 };
+                                        sprintf(disp, "%02d.%02d", current_track, track_count);
+                                        update_display(disp, 1000);
                                 }
-                                
-                                /* read back data */
-                                if(recv(sock , server_reply , 2000 , 0) < 0) {
-                                        syslog(LOG_ERR, "could not retrieve data from socket");
-                                        break;
+
+                                /* Display artist - title */
+                                char artist_title[1000] = { 0x00 };
+                                if(c_artistarray != NULL) {
+                                        char *disp_artist = c_artistarray->valuestring;
+                                        strcpy(artist_title, disp_artist);
                                 }
+
+                                if(c_title != NULL) {
+                                        char *disp_title = c_title->valuestring; 
+                                        strcat(artist_title, " - ");
+                                        strcat(artist_title, disp_title);
+                                } 
+
+                                update_display(artist_title, 1000);
+                                cJSON_Delete(c_root);
+                        }
+
+                        /* if type is video, update display with title */
+                        if(method == METHOD_VIDEO) {
+                                char message[] = "{\"jsonrpc\": \"2.0\", \"method\": \"Player.GetItem\", \"params\": { \"properties\": [\"title\"], \"playerid\": 1 }, \"id\": \"VideoGetItem\"}";
+                                writeReadSocket(sock, message, server_reply);
 
                                 /* Parse JSON data */
                                 c_root = cJSON_Parse(server_reply);
@@ -115,38 +171,13 @@ int main(int argc, char *argv[]) {
                                 if(c_result != NULL) c_item = cJSON_GetObjectItem(c_result,"item");
 
                                 /* Update display with title */
-                                if(method == METHOD_VIDEO) {
-                                        if(c_item != NULL) { 
-                                                c_label = cJSON_GetObjectItem(c_item,"label");
-                                                update_display(c_label->valuestring, 1000); 
-                                        }
+                                if(c_item != NULL) { 
+                                        c_label = cJSON_GetObjectItem(c_item,"label");
+                                        update_display(c_label->valuestring, 1000); 
                                 }
 
-                                /* Update display with artist - title */
-                                if(method == METHOD_AUDIO) {
-                                        if(c_item != NULL) c_artist = cJSON_GetObjectItem(c_item,"artist");
-                                        if(c_item != NULL) c_title = cJSON_GetObjectItem(c_item,"title");
-                                        if(c_artist != NULL) c_artistarray = cJSON_GetArrayItem(c_artist, 0);
-
-                                        char artist_title[1000] = { 0x00 };
-
-                                        if(c_artistarray != NULL) {
-                                                char *disp_artist = c_artistarray->valuestring;
-                                                strcpy(artist_title, disp_artist);
-                                        }
-
-                                        if(c_title != NULL) {
-                                                char *disp_title = c_title->valuestring; 
-                                                strcat(artist_title, " - ");
-                                                strcat(artist_title, disp_title);
-                                        } 
-
-                                        update_display(artist_title, 1000);
-                                }
-
+                                cJSON_Delete(c_root);
                         }
-
-                        cJSON_Delete(c_root);
                 }
 
                 if(paused != 1) { 
@@ -155,15 +186,7 @@ int main(int argc, char *argv[]) {
 
                                 /* Enter endless loop by requesting each second current position */
                                 char message[] = "{\"jsonrpc\": \"2.0\", \"method\": \"Player.GetProperties\", \"params\": { \"properties\": [\"time\"], \"playerid\": 0 }, \"id\": \"AudioGetItem\"}";
-                                if(send(sock, message, strlen(message), 0) < 0) {
-                                        syslog(LOG_ERR, "Could not write to socket");
-                                }
-
-                                /* read back data */
-                                if(recv(sock , server_reply , 2000 , 0) < 0) {
-                                        syslog(LOG_ERR, "could not retrieve data from socket");
-                                        break;
-                                }
+                                writeReadSocket(sock, message, server_reply);
 
                                 /* Parse JSON data */
                                 c_root = cJSON_Parse(server_reply);
@@ -213,6 +236,9 @@ int main(int argc, char *argv[]) {
         }
      
         syslog(LOG_INFO, "caught exit signal - shutting down");
+
+        /* Free server_reply */
+        free(server_reply);
 
         /* Close socket */
         close(sock);
@@ -284,6 +310,17 @@ void update_display(char *text, int delay_ms) {
 
         /* Close file pointer */
         fclose(fp_disp);
+}
+
+int writeReadSocket(int sock, char *message, char *server_reply) { 
+
+        /* Write message to socket */
+        if(send(sock, message, strlen(message), 0) < 0) { return -1; }
+
+        /* read back data */
+        if(recv(sock , server_reply , 2000 , 0) < 0) { return -1; }
+
+        return 0;
 }
 
 void init_exit(int signum) {

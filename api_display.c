f3409203b6b78e13d719e4bd5a753027668eed41
Geert Lorang <geert@lorang.be>
2015-01-21 00:57:43 +0100

    Check startup conditions
    Check if something is playing at startup and/or if the screensaver is enabled.
    In case something is already playing we should start media_info instead of the menu
    In case the screensaver is active we should dim the display

--- api_display.c
+++ api_display.c
@@ -22,13 +22,16 @@
 #include "ad1000.h"
 #include "display.h"
 
+#define DAEMON_MENU "/usr/local/bin/menu"
+#define DAEMON_MEDIA_INFO "/usr/local/bin/media_info"
+
 /* exit on signal */
 volatile sig_atomic_t stop;
 volatile sig_atomic_t paused;
 
 pid_t prev_pid;
 void kill_prev(int parent);
-void fork_menu();
+int fork_daemon(char *cmd);
 void update_brightness(int level);
 
 int main(int argc, char *argv[]) {
@@ -39,11 +42,8 @@ int main(int argc, char *argv[]) {
         char server_reply[2000] = { 0x00 };
 
         /* cJSON vars to parse JSON */
-        cJSON *c_root, *c_method;
+        cJSON *c_root, *c_method, *c_result, *c_screensaver;
         char *method;
-
-        /* pid to fork off media_info helper */
-        pid_t pid;
 
         /* Open syslog */
         openlog("api_display", LOG_PID|LOG_CONS, LOG_USER);
@@ -69,8 +69,62 @@ int main(int argc, char *argv[]) {
         /* Empty display in case there is still something on it for whatever reason */
         update_display("", 0);
 
-        /* Show current selected item on display */
-        fork_menu();
+
+
+
+        /* Check if something is currently playing */
+        char message[] = "{\"jsonrpc\": \"2.0\", \"method\": \"Player.GetActivePlayers\", \"id\": 1}";
+        if(send(sock, message, strlen(message), 0) < 0) { 
+                syslog(LOG_ERR, "could not write to XBMC socket!");
+        } 
+
+        if(recv(sock , server_reply , 2000 , 0) < 0) { 
+                syslog(LOG_ERR, "could not read from XBMC socket!");
+        }
+        
+        /* Parse JSON data to determine if something is playing */
+        c_root = cJSON_Parse(server_reply);
+        if(c_root != NULL) c_result = cJSON_GetObjectItem(c_root,"result");
+
+        /* Nothing is playing - show menu */
+        if(cJSON_GetArraySize(c_result) == 0) { 
+                if(!fork_daemon(DAEMON_MENU)) { 
+                        syslog(LOG_ERR, "could not fork menu daemon!"); 
+                }
+        /* Something is playing - show media info */
+        } else { 
+                if(!fork_daemon(DAEMON_MEDIA_INFO)) { 
+                        syslog(LOG_ERR, "could not fork media_info daemon!"); 
+                }
+        }
+        cJSON_Delete(c_root);
+
+
+
+        /* Check if screensaver currently active or not */
+        char ss_message[] = "{\"jsonrpc\": \"2.0\", \"method\": \"XBMC.GetInfoBooleans\", \"params\": { \"booleans\": [\"System.ScreenSaverActive\"] }, \"id\": 1}";
+        if(send(sock, ss_message, strlen(ss_message), 0) < 0) { 
+                syslog(LOG_ERR, "could not write to XBMC socket!");
+        } 
+
+        if(recv(sock , server_reply , 2000 , 0) < 0) { 
+                syslog(LOG_ERR, "could not read from XBMC socket!");
+        }
+        
+        /* Parse JSON data to check current screensaver state */
+        c_root = cJSON_Parse(server_reply);
+        if(c_root != NULL) c_result = cJSON_GetObjectItem(c_root,"result");
+        if(c_result != NULL) c_screensaver = cJSON_GetObjectItem(c_result,"System.ScreenSaverActive");
+        if(c_screensaver->type == cJSON_True) {
+                update_brightness(0);
+        } else {
+                update_brightness(2);
+        }
+
+        cJSON_Delete(c_root);
+
+
+
 
         /* Main loop - Wait for data to arrive */    
         while(stop == 0) { 
@@ -84,40 +138,43 @@ int main(int argc, char *argv[]) {
                 if(c_root != NULL) c_method = cJSON_GetObjectItem(c_root, "method");
                 if(c_method != NULL) method = c_method->valuestring;
 
-                /* Check what event came in and update display as needed */
+                /* Check what event came in and act as needed */
                 if(strcmp(method, "Playlist.OnClear") == 0) {
+
                         /* terminater media_info or menu process */
                         kill_prev(getpid());
-               } else if(strcmp(method, "Player.OnPlay") == 0) {
+
+                } else if(strcmp(method, "Player.OnPlay") == 0) {
 
                         kill_prev(getpid());
-
-                        pid = fork();
-                        if(pid == -1) {
-                                syslog(LOG_ERR, "fork failed!");
-                        }
-
-                        if(pid == 0) {
-                                char *cmd = "/usr/local/bin/media_info";
-                                char *cmd_args[] = { cmd, NULL};
-                                execvp(cmd, cmd_args);
-                                _exit(0);
-                        } else { 
-                                prev_pid = pid;
+                        if(!fork_daemon(DAEMON_MEDIA_INFO)) { 
+                                syslog(LOG_ERR, "could not fork media_info daemon!"); 
                         }
 
                 } else if(strcmp(method, "Player.OnPause") == 0) {
+
                         kill(prev_pid, SIGUSR1); 
                         update_display("PAUS", 1000);
                         paused = 1;
+
                 } else if(strcmp(method, "Player.OnStop") == 0) {
+
                         kill_prev(getpid());
                         update_display("STOP", 1000);
                         update_display("", 0);
-                        fork_menu();
+
+                        /* Show current selected item on display */
+                        if(!fork_daemon(DAEMON_MENU)) {
+                                syslog(LOG_ERR, "could not fork menu daemon!");
+                        }
+
+
                 } else if(strcmp(method, "GUI.OnScreensaverActivated") == 0) {
+
                         update_brightness(0);
+
                 } else if(strcmp(method, "GUI.OnScreensaverDeactivated") == 0) {
+
                         update_brightness(2);
                 }
 
@@ -138,17 +195,22 @@ int main(int argc, char *argv[]) {
         exit(EXIT_SUCCESS);
 }
 
-void fork_menu() {
-
+int fork_daemon(char *cmd) {
         pid_t pid = fork();
+
+        if(pid == -1) {
+                return -1;
+        }
+
         if(pid == 0) {
-                char *cmd = "/usr/local/bin/menu";
                 char *cmd_args[] = { cmd, NULL};
                 execvp(cmd, cmd_args);
                 _exit(0);
         } else { 
                 prev_pid = pid;
         }
+
+        return 0;
 }
 
 void init_exit(int signum) {
